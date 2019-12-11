0d465a8999757b92a5dcdf7b53c2f7a3e89ade2d
subpyrus <diogoxambre.gouveia@gmail.com>
2019-12-11 11:20:38 +0000

    commit imagens

--- projeto_lab/src/pages/PokemonTrivia.js
+++ projeto_lab/src/pages/PokemonTrivia.js
@@ -111,7 +111,7 @@ class PokemonTrivia extends Component {
                         </Col>
                     </Col>
                     <Col className='text-center' xs='12' md='4' lg='5'>
-                        <img className='img-fluid py-2 py-md-0' src='' alt='alakazamTrivia' />
+                        <img className='img-fluid py-2 py-md-0' src={require('../imagens/alakazam.png')} alt='alakazamTrivia' />
                     </Col>
                 </Row>
             )
